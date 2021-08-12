ee4212e6d591ed2d901c2b6bdf0d340d3e06d00f
Nishad <root@ip-172-31-9-0.ap-south-1.compute.internal>
2021-08-12 11:43:07 +0000

    Added a select case script to test parameters

---
+++ para-monthcase.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Enter a number"
+read n
+case $n in
+	1)
+    echo "January" ;;
+    2)
+    echo "February" ;;
+    3)
+    echo "March" ;;
+    4) 
+    echo "April" ;;
+    5)
+    echo "May" ;;
+    6)
+    echo "June" ;;
+    7)
+    echo "July" ;;
+    8) 
+    echo "August" ;;
+    9)
+    echo "September" ;;
+    10)
+    echo "October" ;;
+    11)
+    echo "November" ;;
+    12)
+    echo "December" ;;
+    *)
+    echo "Please enter a valid number between 0 to 12" ;;
+esac
